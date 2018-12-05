9ac2193a896271c8ee0472ac14534c3c863bf5df
Scott Longstreet <slongstreet@hotmail.com>
2018-12-04 20:06:23 -0800

    Solved day03 part 2

--- day03/main.go
+++ day03/main.go
@@ -21,20 +21,37 @@ func main() {
 
 	// Part 1 - calculate number of square inches with multiple claims
 	multipleClaims := make(map[string]bool) // store multi claims as we go to avoid multiple enumerations
+	overlapCheck := make([]int, len(squares)+1)
 
 	var grid [1000][1000]int
 	for _, sq := range squares {
 		for i := sq.X; i < sq.X+sq.Width; i++ {
 			for j := sq.Y; j < sq.Y+sq.Height; j++ {
-				grid[i][j]++
-				if grid[i][j] > 1 {
+				if grid[i][j] == 0 {
+					grid[i][j] = sq.ID // if grid location is unused, store our ID here.
+				} else {
+					// grid location is shared, so mark it as having multiple claims
 					multipleClaims[strconv.Itoa(i)+","+strconv.Itoa(j)] = true
+					overlapCheck[max(grid[i][j], 0)] = 1 // store that this square has an overlap...
+					overlapCheck[sq.ID] = 1              // ...and the current square too.
+					grid[i][j] = -1
 				}
 			}
 		}
 	}
 
 	fmt.Printf("Square inches with multiple claims: %d.\n", len(multipleClaims))
+
+	// Part 2 - find the only claim which doesn't overlap
+	var nonoverlappingID int
+	for id, val := range overlapCheck {
+		if val == 0 {
+			nonoverlappingID = id
+			break
+		}
+	}
+
+	fmt.Printf("Non-overlapping claim ID: %d\n", nonoverlappingID)
 }
 
 // Reads and parses square definitions from a file.
@@ -71,4 +88,12 @@ func parseInputLine(input string) square {
 	sq.Height, _ = strconv.Atoi(dimensions[1])
 
 	return sq
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
